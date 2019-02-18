fd20e7b0862c5180f9230875bfa94592c0848a9f
biscuitmanz <doddsanater87@gmail.com>
2019-02-18 22:30:36 +0000

    finished ball and paddle

--- main.js
+++ main.js
@@ -1,32 +1,104 @@
 let canvas = document.querySelector('canvas');
-let dpi = window.devicePixelRatio;
 let ctx = canvas.getContext('2d');
 
+//ball variables
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+let ballRadius = 10;
+
+//paddle variables
+let paddleHeight = 10;
+let paddleWidth = 80;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight;
+
+//keyboard variables
+let rightPressed = false;
+let leftPressed = false;
+
+//brick variables
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
 
 
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
+    ctx.fillStyle = '#2E0927';
+    ctx.fill();
+    ctx.closePath();
+}
 
-    let x = 50;
-    let y = 50;
-    let r = 10;
-   let speed = 1;
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight)
+    ctx.fillStyle = '#2E0927';
+    ctx.fill();
+    ctx.closePath();
+}
 
-    function animate(){
-        ctx.beginPath();
-        ctx.clearRect(0, 0, innerWidth, innerHeight);
-        ctx.arc(x, y, r, 0, Math.PI*2, false);
-        ctx.stroke();
-        ctx.strokeStyle = '#000';
-        if(x > canvas.width -r/2 || x < 0 +r/2){
-            speed = -speed;
-        }
-        x += speed;
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle()
+    x += dx;
+    y += dy;
+    
 
-
-        requestAnimationFrame(animate);
+    //ball bounce off walls
+    if(x + dx > canvas.width -ballRadius || x + dx < ballRadius) {
+        dx = -dx;
     }
 
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if (y + dy > canvas.height - ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth){
+          dy = -dy;
+      }else{
+        alert('badmanz')
+        // document.location.reload();
+        clearInterval(interval);
+      }
+    }   
 
-    animate();
-    
-    
+    //paddle move left and right
+    if(rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -=7;
+    }
+}
 
+
+function keyDownHandler(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+    }
+    else if(e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+let interval = setInterval(draw, 10)
